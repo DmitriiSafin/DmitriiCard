794c306bb6fbe1aed893a73c51a64f47cf1dc1a3
Dmitry Safin <developersafin@gmail.com>
2023-03-17 19:11:37 +0500

    The App is done

--- DmitriiCard/ContentView.swift
+++ DmitriiCard/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("Dmitrii")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle()
+                            .stroke(Color.white, lineWidth: 5)
+                    }
+                Text("Dmitrii Safin")
+                    .font(.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "9-937-15-16-203", imageName: "phone.fill")
+                InfoView(text: "safin-dm@yandex.ru", imageName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
