6c5bdb83eb94140636f3d9dd3827d32f8b0290bc
Dmitry Safin <developersafin@gmail.com>
2023-03-17 18:02:41 +0500

    Initial Commit

---
+++ DmitriiCard/DmitriiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DmitriiCardApp.swift
+//  DmitriiCard
+//
+//  Created by Дмитрий on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DmitriiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
